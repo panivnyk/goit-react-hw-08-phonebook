d9ae55d95ba92e3fd6829c2c81f65428c0746a70
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-11-06 21:01:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import './index.css';
 
 import { Provider } from 'react-redux';
 // import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store';
+// import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
