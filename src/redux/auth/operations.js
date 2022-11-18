216c9fe8c5eca8dd3b759d676d7abf4815d20754
Dmitriy Panivnyk <d.panivnyk@ukr.net>
2022-11-18 23:48:42 +0200

    fix refreshuser page

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -82,7 +82,7 @@ export const refreshUser = createAsyncThunk(
     try {
       // If there is a token, add it to the HTTP header and perform the request
       setAuthHeader(persistedToken);
-      const response = await axios.get('/users/me');
+      const response = await axios.get('/users/current');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
